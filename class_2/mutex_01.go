b70118a0a62ef9ba8c24f51b5704099ab0c69929
jck0929 <35943397+jck0929@users.noreply.github.com>
2021-02-05 01:35:54 +0800

    Create mutex_01.go
    mutex第一版源码示例

---
+++ class_2/mutex_01.go
@@ -0,0 +1,44 @@
+package mutex_src
+
+// CAS操作，当时还没有atomic包
+func cas(val *int32, old, new int32) bool {}
+func semacquire(*int32)                   {}
+func semrelease(*int32)                   {}
+
+// 互斥锁的结构
+type Mutex struct {
+	key  int32 // 锁是否被持有的标致
+	sema int32 // 信号量专用，用以唤醒/阻塞goroutine
+}
+
+// 保证成功在val上增加delta的值
+func xadd(val *int32, delta int32) (new int32) {
+	for {
+		v := *val
+		if cas(val, v, v+delta) {
+			return v + delta
+		}
+
+		panic("unreached")
+	}
+}
+
+// 请求锁
+func (m *Mutex) Lock() {
+	// 标识+1，如果等于1，则成功获取到锁
+	if xadd(&m.key, 1) == 1 {
+		return
+	}
+	// 否则阻塞等待
+	semacquire(&m.sema)
+}
+
+// 释放锁
+func (m *Mutex) UnLock() {
+	// 标识-1，如果等于0，则没有其他等待者
+	if xadd(&m.key, -1) == 0 {
+		return
+	}
+	// 否则唤醒其他等待者
+	semrelease(&m.sema)
+}
